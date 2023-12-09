8cf0f76334cc6ac18529d781f8d6e9dd129cb4e3
Samet SARIKAYA <13sametsarikaya@gmail.com>
2023-12-09 03:52:22 +0300

    Game and Card updates
    new functions and new methods

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,4 @@
 public class Card {
-    final boolean MINUS = false;
-    final boolean PLUS = true;
     private int value;
     private String color;
     private boolean sign;
@@ -15,26 +13,46 @@ public class Card {
         this.isDouble = isDouble;
     }
 
+    public void setCard(int value, String color, boolean sign, boolean isFlip, boolean isDouble) {
+        this.value = value;
+        this.color = color;
+        this.sign = sign;
+        this.isFlip = isFlip;
+        this.isDouble = isDouble;
+    }
+
+    public void freeCard(){
+        this.value = 0;
+        this.color = null;
+        this.sign = false;
+        this.isFlip = false;
+        this.isDouble = false;
+    }
+
+    public String getCardInfo(){
+        if(!isFlip && !isDouble) {
+            return this.getColor() + " " + this.getValue();
+        }else if(isFlip){
+            return "-/+";
+        }else{
+            return "x2";
+        }
+    }
+
     public int getValue() {
-        return value;
+        if(sign){ return -1*value;}
+        else {return value;}
+    }
+    public void doubleValue(){
+        this.value *= 2;
     }
 
-    public String getColor() {
-        return color;
-    }
+    public String getColor() {return color;}
 
-    public boolean getSign() {
-        return sign;
-    }
+    public boolean getSign() {return sign;}
+    public void setSign(boolean sign) {this.sign = sign;}
 
-    public boolean isFlip() {
-        return isFlip;
-    }
+    public boolean isFlip() {return isFlip;}
 
-    public boolean isDouble() {
-        return isDouble;
-    }
-    public void setSign(boolean newSign){
-        this.sign = newSign;
-    }
+    public boolean isDouble() {return isDouble;}
 }
--- src/Game.java
+++ src/Game.java
@@ -1,88 +1,125 @@
-//imports
+import java.util.Arrays;
 import java.util.Random;
-public class Game{
+
+public class Game {
     String[] cardColors = {"Red", "Blue", "Yellow", "Green"};
     Random random = new Random();
     Card[] gameDeck = new Card[40];
     Card[] playerDeck = new Card[10];
     Card[] computerDeck = new Card[10];
-    private Card[] remove(Card[] deck, int index){
 
-        Card[] newDeck = new Card[deck.length-1];
-        for(int i=0, k=0;i<deck.length;i++) {
+    public Game(){
+        initDecks();
+//        System.out.println(Arrays.toString(computerDeck));
+        for (int i=0;i<=39;i++) {
+            System.out.println(gameDeck[i].getCardInfo());
+        }
+        System.out.println("########");
+        for (int i=0;i<=9;i++) {
+            System.out.println(playerDeck[i].getCardInfo());
+        }
+        System.out.println("########");
+        for (int i=0;i<=9;i++) {
+            System.out.println(computerDeck[i].getCardInfo());
+        }
+//        System.out.println(Arrays.toString(playerDeck));
+//        System.out.println(Arrays.toString(gameDeck));
+
+    }
+
+    private void arrayCopy(Card[] deck,Card[] newDeck){
+        for(int i=0; i<deck.length;i++){
+            if(i<newDeck.length) {deck[i] = newDeck[i];} else {deck[i].freeCard();}
+        }
+    }
+
+    private void removeCard(Card[] deck, int index) {
+        Card[] newDeck = new Card[deck.length - 1];
+        for (int i = 0, k = 0; i < deck.length; i++) {
             if (i != index) {
                 newDeck[k] = deck[i];
                 k++;
             }
         }
-        deck = newDeck;
+        arrayCopy(gameDeck, newDeck);
     }
-
-    public Card RemoveCard() {
+    private Card useAndRemoveCard(Card[] deck) {
         Card tempValue = deck[0];
         Card[] newDeck = new Card[deck.length - 1];
-        for (int i = 1; i < deck.length; i++) {
-            newDeck[i - 1] = deck[i];
-        }
-        deck = newDeck;
-        return temp;
+        for(int i = 0; i < deck.length-1; i++) {newDeck[i] = deck[i+1];}
+        arrayCopy(gameDeck, newDeck);
+        return tempValue;
     }
-    public static Card[] shuffle(Card[] Deck) {
-        for (int i = Deck.length - 1; i > 0; i--) {
-            int index = random.nextInt(i + 1);
-            Card tempValue = Deck[index];
-            Deck[index] = Deck[i];
-            Deck[i] = tempValue;
-        }
-        return Deck;
-    }
-    private void initializeDecks() {
-        for (int i = 1; i <= 10; i++) {
+    private void create_gameDeck() {
+        for (int i = 1, k = 0; i <= 10; i++) {
             for (String color : cardColors) {
-                gameDeck.add(new Card(i, color, PLUS, false, false));
+                gameDeck[k] = new Card(i, color, false, false, false);
+                k++;
             }
         }
 
-        shuffle(gameDeck);
-
-        // Fill player and computer decks with 10 cards each
-        for (int i = 0; i < 5; i++) {
-            computerDeck[i] = gameDeck[i];
-            playerDeck[i] = gameDeck[39 - i];
-        }
-
-        // Add three cards with random colors and values between 1 to 6, with an additional sign
-        for (int i = 5; i < 8; i++) {
-            int value;
-            String color;
-            do {
-                index = random.nextInt(40);
-                value = gameDeck[index].getValue();
-                color = gameDeck[index].getColor();
-            } while (value >= 6);
-            boolean sign = random.nextBoolean();
-            computerDeck[index] = new Card(value, color, sign, false, false);
-
-            do {
-                index = random.nextInt(40);
-                value = gameDeck[index].getValue();
-                color = gameDeck[index].getColor();
-            } while (value >= 6);
-            sign = random.nextBoolean();
-            playerDeck[index] = new Card(value, color, sign, false, false);
-        }
-
-        // Add the remaining two cards
-        if (random.nextInt(5) == 0) {
-            Card card1 = new Card(0, null, false, true, false);
-            Card card2 = new Card(0, null, false, false, true);
-            gameDeck.add(card1);
-            gameDeck.add(card2);
-        } else {
-            String color = cardColors[random.nextInt(cardColors.length)];
-            int value = random.nextInt(6) + 1;
-            boolean isSigned = random.nextBoolean();
-            gameDeck.add(new Card(value, color, isSigned, false, false));
-            gameDeck.add(new Card(value, color, isSigned, false, false));
+    }
+    private void shuffle(Card[] deck) {
+        for (int i = 0; i < 150; i++) {
+            int f = random.nextInt(deck.length);
+            int k = random.nextInt(deck.length);
+            Card temp = deck[k];
+            deck[k] = deck[f];
+            deck[f] = temp;
         }
     }
+    private void createFirstFiveCards() {
+        for (int i = 0; i < 5; i++) {
+            computerDeck[i] = useAndRemoveCard(gameDeck);
+            playerDeck[i] = gameDeck[gameDeck.length - 1];
+            removeCard(gameDeck, gameDeck.length - 1);
+        }
+    }
+    private void createThreeCards(Card[] deck) {
+        for (int i = 5; i < 8; i++) {
+            int value, index;
+            String color;
+            do {
+                index = random.nextInt(deck.length-i);
+                value = gameDeck[index].getValue();
+                color = gameDeck[index].getColor();
+            } while (value > 6);
+            removeCard(gameDeck, index);
+            boolean sign = random.nextBoolean();
+            deck[i] = new Card(value, color, sign, false, false);
+        }
+    }
+    private void createLastTwoCard(Card[] deck) {
+        if (random.nextInt(5) == 0) {
+            Card card = new Card(0, null, false, true, false);
+            deck[8] = card;
+        }else {
+            boolean sign = random.nextBoolean();
+            deck[8] = useAndRemoveCard(gameDeck);
+            deck[8].setSign(sign);
+        }
+        if (random.nextInt(5) == 0) {
+            Card card = new Card(0, null, false, false, true);
+            deck[9] = card;
+        }else {
+            boolean sign = random.nextBoolean();
+            deck[9] = useAndRemoveCard(gameDeck);
+            deck[9].setSign(sign);
+        }
+    }
+    public void initDecks(){
+        for (int i = 1, k =0; i <= 10; i++) {
+            for (String color : cardColors) {
+                gameDeck[k] = new Card(i, color, false, false, false);
+                k++;
+            }
+        }
+        create_gameDeck();
+        shuffle(gameDeck);
+        createFirstFiveCards();
+        createThreeCards(computerDeck);
+        createThreeCards(playerDeck);
+        createLastTwoCard(computerDeck);
+        createLastTwoCard(playerDeck);
+    }
+}
